166da68af863593d59bf79f4a6ad79c81a1327b8
Christophe Soula <christophe.soula@xpirience.ch>
2024-12-13 09:55:19 +0100

    draft(web): debug

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?>XXX</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- Menu de Navigation -->
+<nav class="navbar">
+    <div class="container">
+        <a href="index.php" class="logo">xPirience</a>
+        <ul class="nav-links">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="about.php">À propos</a></li>
+            <li><a href="solution.php">Solution</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </div>
+</nav>
